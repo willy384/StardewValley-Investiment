08166180ae90e26df7f4569a15d10fa46c1be6f2
Peterson <petersonmqt@gmail.com>
2025-06-10 13:32:56 -0300

    Add files via upload

---
+++ calc_por_colheita.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+
+int dinheiro,venda,compra,lucro,custo,quantidade,inventario(0);
+
+int main(){
+
+    std::cout << "Dinheiro ";
+    std::cin >> dinheiro;
+    std::cout << "Custo ";
+    std::cin >> custo;
+    std::cout << "Lucro ";
+    std::cin >> lucro;
+
+    quantidade=(dinheiro/custo);
+    std::cout << "Voce pode comprar " << quantidade << " unidades"<< std::endl;
+    std::cout <<  "Quantos quer? ";
+
+    do{
+        std::cin >> compra;
+    }while(compra >= quantidade && compra < 0);
+    std::cout << "Possui algum no inventario ";
+    std::cin >> inventario;
+    quantidade=compra;
+    compra=quantidade*custo;
+    dinheiro-=compra;
+    venda=(custo+lucro)*(quantidade+inventario);
+    dinheiro+=venda;
+
+    //imprimre resultado
+    std::cout << "Voce gastou " << compra << " em " << quantidade << " unidades e tinha " << inventario << " unidades, somando um total de " << quantidade+inventario << std::endl; 
+    std::cout << "Vendeu "<< quantidade+inventario << " unidades por " << custo+lucro << " cada " << std::endl; 
+    std::cout << "Terminou o dia com o lucro de " << lucro*(quantidade+inventario) << std::endl;
+    std::cout << "Saldo atual "<< dinheiro << std::endl;
+    system("pause");
+    system("cls");
+
+return 0;
+}
